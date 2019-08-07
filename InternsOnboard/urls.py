43b9447774cd698af2d2252557837399a083938c
Kiran <sai8kiran99@gmail.com>
2019-08-07 22:34:01 +0530

    Apply still not working

--- InternsOnboard/urls.py
+++ InternsOnboard/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path,include
 from userRegister import views as user_views
 from django.conf import settings
+from studentPortal import views as student_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +15,5 @@ urlpatterns = [
     path('', include('InternsOnboardMain.urls')),
     path('', include('coordinatorPortal.urls')),
     path('api/posts/', include('coordinatorPortal.urls')),
+    path('apply/',student_views.apply,name='student-apply')
 ]
--- studentPortal/views.py
+++ studentPortal/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse
+from .models import studentInternship
+from .forms import studentInternshipForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 
-# Create your views here.
+# def apply(request):
+#     if request.GET.get('applybtn'):
+#         profil = get_object_or_404(studentInternship)
+#         profil.applied = True
+#         profil.save(update_fields=["applied"])
+#         # profil.create(applied=True)
+#         return render(request, 'InternsOnboard-Home')
+#     return render(request, 'InternsOnboardMain/home.html')
+
+def apply(request):
+    if studentInternship.applied:
+        studentinternship = studentInternship(
+            applied=True
+        )
+        studentinternship.save()
+        messages.success(request, 'Applied!')
+    else:
+        messages.warning(request,'You have already applied!')
+    return redirect('InternsOnboard-Home')
+
+            
