03dfe7c35bd97bdfcd8b0fdbbe8fc177208c3d05
vineeth <mailtovineethram@gmail.com>
2019-08-10 20:59:15 +0530

    hello

--- studentPortal/models.py
+++ studentPortal/models.py
@@ -3,6 +3,9 @@ from InternsOnboardMain.models import internshipPost
 from django.contrib.auth.models import User
 
 class studentInternship(models.Model):
-    companyName = models.ForeignKey(internshipPost,on_delete=models.CASCADE,null=True)
-    studentName = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    companyName = models.ForeignKey(internshipPost,on_delete=models.CASCADE,null=True,db_column='companyName')
+    studentName = models.ForeignKey(User,on_delete=models.CASCADE,null=True,db_column='studentName')
     applied = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'studentInternship'
--- studentPortal/views.py
+++ studentPortal/views.py
@@ -6,19 +6,28 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from InternsOnboardMain.models import internshipPost
+from django.db.models import Q
 
 @login_required
 def apply(request):
     if "applybtn" in request.POST:
-        # currentStudentName = request.user
-        # c = request.POST['c_name']
-        # currentCompany=internshipPost.objects.get(company_name=c)
-        # studentinternship = studentInternship(
-        #     studentName=currentStudentName,
-        #     applied=True,
-        #     companyName= currentCompany
-        # )
-        messages.success(request, 'Applied!')
+        currentStudentName = request.user
+        c = request.POST['c_name']
+        currentCompany=internshipPost.objects.get(company_name=c)
+        try:
+            p=studentInternship.objects.get(companyName__exact=currentCompany,studentName__exact=currentStudentName)
+            if p:
+                if p.applied==True:
+                    messages.warning(request, 'Already applied!')
+                elif p.applied==False:
+                    p.applied=True
+        except studentInternship.DoesNotExist :
+            studentinternship = studentInternship(
+                studentName=currentStudentName,
+                applied=True,
+                companyName= currentCompany
+            )
+            messages.success(request, 'Applied!')
     elif "rejectbtn" in request.POST:
         messages.warning(request, 'Rejected')
     return redirect('InternsOnboard-Home')
