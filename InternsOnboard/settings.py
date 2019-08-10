eea1cad075738359e1d31490cd479d2d42b36365
vineeth <mailtovineethram@gmail.com>
2019-08-10 16:08:45 +0530

    home page

--- InternsOnboard/settings.py
+++ InternsOnboard/settings.py
@@ -69,15 +69,8 @@ WSGI_APPLICATION = 'InternsOnboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
-        'NAME': 'internsonboardDB',
-        'USER': 'root',
-        'PASSWORD': 'kiran8499',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- studentPortal/views.py
+++ studentPortal/views.py
@@ -9,15 +9,16 @@ from InternsOnboardMain.models import internshipPost
 
 @login_required
 def apply(request):
-    currentStudentName = request.user
-    c = request.POST['c_name']
-    currentCompany=internshipPost.objects.get(company_name=c)
-    studentinternship = studentInternship(
-        studentName=currentStudentName,
-        applied=True,
-        companyName= currentCompany
-    )
-
-    studentinternship.save()
-    messages.success(request, 'Applied!')
+    if "applybtn" in request.POST:
+        # currentStudentName = request.user
+        # c = request.POST['c_name']
+        # currentCompany=internshipPost.objects.get(company_name=c)
+        # studentinternship = studentInternship(
+        #     studentName=currentStudentName,
+        #     applied=True,
+        #     companyName= currentCompany
+        # )
+        messages.success(request, 'Applied!')
+    elif "rejectbtn" in request.POST:
+        messages.warning(request, 'Rejected')
     return redirect('InternsOnboard-Home')
